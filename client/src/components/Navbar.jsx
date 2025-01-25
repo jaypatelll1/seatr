8bdc9e809de8d362cd339d49e28e97c0543472f8
jay patel <pateljay8886@gmail.com>
2025-01-26 01:30:55 +0530

    Refactor Navbar component to use Redux for userName and email; clean up code formatting

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.svg";
-function Navbar({ userName,email }) {
+
+function Navbar() {
   const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
   const userDropdownRef = useRef(null);
+
+  // Get userName and email from Redux
+  const userName = useSelector((state) => state.user.name);
+  const email = useSelector((state) => state.user.email);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -22,25 +28,21 @@ function Navbar({ userName,email }) {
   return (
     <div>
       <nav className="bg-white border-gray-200 shadow-lg">
-        <div className="w-screen-xl flex items-center justify-between  p-2">
+        <div className="w-screen-xl flex items-center justify-between p-2">
           {/* Logo Section */}
           <a
             href="https://flowbite.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src={logo}
-              className="h-8"
-              alt="Flowbite Logo"
-            />
+            <img src={logo} className="h-8" alt="Flowbite Logo" />
           </a>
 
           {/* Search Bar */}
           <div className="flex-1 max-w-lg ml-4">
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400 "
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -57,7 +59,7 @@ function Navbar({ userName,email }) {
               </div>
               <input
                 type="text"
-                className="block w-full p-2 pl-10 text-sm text-gray-900  border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 pl-10 text-sm text-gray-900 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
               />
             </div>
@@ -78,9 +80,7 @@ function Navbar({ userName,email }) {
               />
             </button>
             <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 ">
-                {userName}
-              </span>
+              <span className="block text-sm text-gray-900">{userName}</span>
             </div>
 
             {/* User Dropdown */}
@@ -95,15 +95,6 @@ function Navbar({ userName,email }) {
                   </span>
                 </div>
                 <ul className="py-2">
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-
                   <li>
                     <a
                       href="#"
